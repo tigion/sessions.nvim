1418a47b5d83da1577fd1063e6881df14c343685
Christoph Zirkelbach <christoph@tigion.de>
2026-04-14 13:39:14 +0200

    refactor(health): update the check messages

--- lua/sessions/health.lua
+++ lua/sessions/health.lua
@@ -14,38 +14,59 @@ local error = vim.health.error -- Reports an error.
 ---Checks the data directory.
 local function check_data_directory()
   local data_dir = vim.fn.stdpath('data')
-  local message = 'Data directory `' .. data_dir .. '`'
+  local message = 'Data directory'
+  local message_location = '\nLocation: `' .. data_dir .. '`'
 
   -- Checks if it exists.
   if vim.fn.isdirectory(data_dir) ~= 1 then
-    error(message .. ' does not exist')
+    error(message .. ' does not exist' .. message_location)
     return
   end
 
   -- Checks if it is writable.
   if vim.fn.filewritable(data_dir) == 2 then
-    ok(message .. ' is writable')
+    ok(message .. ' is writable' .. message_location)
   else
-    error(message .. ' is not writable')
+    error(message .. ' is not writable' .. message_location)
   end
 end
 
 ---Checks the session directory.
+---@return boolean True if the session directory exists, false otherwise.
 local function check_session_directory()
   local session_dir = session.directory()
-  local message = 'Session directory `' .. session_dir .. '`'
+  local message = 'Session directory'
+  local message_location = '\nLocation: `' .. session_dir .. '`'
+  local message_note = '\nThis is normal if no sessions have been saved yet.'
 
   -- Checks if it exists.
   if vim.fn.isdirectory(session_dir) ~= 1 then
-    warn(message .. ' does not exist' .. '\n' .. 'This is normal if no sessions have been saved yet.')
-    return
+    warn(message .. ' does not exist' .. message_location .. message_note)
+    return false
   end
 
   -- Checks if it is writable.
   if vim.fn.filewritable(session_dir) == 2 then
-    ok(message .. ' is writable')
+    ok(message .. ' exists and is writable' .. message_location)
   else
-    error(message .. ' is not writable')
+    error(message .. ' exists, but is not writable' .. message_location)
+  end
+
+  return true
+end
+
+---Checks the session file for the current working directory.
+local function check_current_session()
+  local filename = session.filename()
+  local message = 'Session file for current working directory'
+  local message_file = '\nFile: `' .. filename .. '`'
+  local message_note = '\nThis is normal if no session has been saved yet.'
+
+  -- Shows if the session file exists.
+  if session.exists() then
+    ok(message .. ' exists' .. message_file)
+  else
+    info(message .. ' does not exist' .. message_file .. message_note)
   end
 end
 
@@ -57,29 +78,17 @@ local function check_sessions()
   -- Shows the count of session files.
   local session_files = vim.fn.globpath(session_dir, expr, true, true)
   local count = #session_files
-  info('Session files: ' .. count)
-end
-
----Checks the session file for the current working directory.
-local function check_current_session()
-  local session_filepath = session.filepath()
-  local filename = vim.fs.basename(session_filepath)
-
-  -- Shows if the session file exists.
-  if vim.fn.filereadable(session_filepath) == 1 then
-    info('Session file `' .. filename .. '` for current working directory exists')
-  else
-    info('No session file for current working directory')
-  end
+  info('Found ' .. count .. ' session file' .. (count ~= 1 and 's' or '') .. ' in the session directory.')
 end
 
 ---Checks the health of the plugin.
 function M.check()
   start('sessions.nvim')
   check_data_directory()
-  check_session_directory()
-  check_sessions()
-  check_current_session()
+  if check_session_directory() then
+    check_current_session()
+    check_sessions()
+  end
 end
 
 return M
